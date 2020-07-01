424780e10c23658cea4442baee880af108be9e47
kostiantynkushch <kostiantyn.kushch@gmail.com>
2020-07-02 00:58:49 +0300

    1st commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <li>
+    <span :class="{done: todo.completed}">
+      <input type="checkbox" @change="todo.completed = !todo.completed" />
+      <strong>{{index + 1}}</strong>
+      {{todo.title | uppercase}}
+    </span>
+    <button class="rm" @click="$emit('remove-todo', todo.id)">&times;</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    },
+    index: Number
+  },
+  filters: {
+    uppercase(value) {
+      return value.toUpperCase();
+    }
+  }
+};
+</script>
+
+<style scoped>
+li {
+  border: 1px solid #ccc;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 2rem;
+  margin-bottom: 1rem;
+}
+.rm {
+  background: red;
+  color: #fff;
+  border-radius: 50%;
+  font-weight: bold;
+}
+input {
+  margin-right: 1rem;
+  cursor: pointer;
+}
+.done {
+  text-decoration: line-through;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>Home Page</h2>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos mollitia nam alias.</p>
+
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
